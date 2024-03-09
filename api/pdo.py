9f2d73c1358343a7f3caf412cc074d71cd8a8ea0
Marcio Junior <marciohamer@gmail.com>
2024-03-08 21:08:55 -0300

    v10 on fire

--- api/pdo.py
+++ api/pdo.py
@@ -24,32 +24,31 @@ class PDO:
         self.db.execute_query(query, (novo_saldo, cliente_id), operation = 'update')
 
     def get_extrato(self, id):
-        cliente = self.get_cliente(id)
+        query = f"SELECT c.saldo_inicial, c.limite, tc.valor,tc.transacao_tipo,descricao,realizado_em FROM clientes c inner join transacoes_cliente tc on tc.cliente_id = {id} order by tc.realizado_em desc limit 10"
+        results = self.db.execute_query(query, (id,))
         
-        if not cliente:
+        if not results:
             return None
         
-        for saldo_cliente, limite_cliente in cliente:
-            saldo = saldo_cliente
-            limite = limite_cliente
-                
-        transacoes = self.get_transacoes(id)
-
-        extrato = {
-            "saldo": {
-                "total": saldo,
+        saldo = {}
+        ultimas_transacoes = []
+        
+        for saldo_inicial, limite, valor, transacao_tipo, descricao, realizado_em in results:
+            saldo = {
+                "total": saldo_inicial,
                 "data_extrato": datetime.now().isoformat(),
                 "limite": limite
-            },
-            "ultimas_transacoes": []
+            }
+            ultimas_transacoes.append({
+                "valor": valor,
+                "tipo": transacao_tipo,
+                "descricao": descricao,
+                "realizado_em": realizado_em.isoformat()
+            })
+            
+        objetao = {
+            "saldo": saldo,
+            "ultimas_transacoes": ultimas_transacoes
         }
 
-        for valor, tipo, descricao, realizado_em in transacoes:
-            extrato["ultimas_transacoes"].append({
-                "valor": valor,
-                "tipo": tipo,
-                "descricao": descricao,
-                "realizada_em": realizado_em.isoformat()
-            })
-
-        return extrato
+        return objetao
